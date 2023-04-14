859927616352c18ad1e15c26d5e1d347c2264b30
Rushikesh Deshmukh <rushikeshcodes@gmail.com>
2023-04-14 13:31:53 +0530

    created header component

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Offers from './pages/Offers';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import ForgotPassword from './pages/ForgotPassword';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      <Header />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/profile' element={<Profile />} />
